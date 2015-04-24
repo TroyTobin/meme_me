3c2951daa086f2657282ce3cd26293fd6a0576e5
Troy Tobin <ttobin@Troys-MacBook-Pro.local>
2015-04-24 22:12:41 +0930

    Added text fields for top and bottom meme text.  Move view up the height of the keyboard for the bottom text field

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,13 +9,47 @@
 import UIKit
 import MobileCoreServices
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate {
 
   @IBOutlet weak var ImageView: UIImageView!
+  @IBOutlet weak var TopTextField: UITextField!
+  @IBOutlet weak var BottomTextField: UITextField!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    // Subscribe to keyboard notifications to allow the view to raise when necessary
+    self.subscribeToKeyboardNotifications()
+    BottomTextField.delegate = self
+    TopTextField.delegate = self
+  }
+  
+  func subscribeToKeyboardNotifications() {
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+  }
+  
+  func keyboardWillShow(notification: NSNotification) {
+    if BottomTextField.isFirstResponder() {
+      self.view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+  }
+  
+  func textFieldShouldReturn(textField: UITextField) -> Bool {
+    if BottomTextField.isFirstResponder() {
+      self.view.frame.origin.y = 0
+    }
+    self.view.endEditing(true)
+    return true
+  }
+  
+  func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+    let userInfo = notification.userInfo
+    let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+    return keyboardSize.CGRectValue().height
   }
   
   @IBAction func OpenAlbum(sender: AnyObject) {
